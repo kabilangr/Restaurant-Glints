ffd46b6f100911acf59ca27d37ed30fdcf6e7108
kabilangr <g.r.kabilan@gmail.com>
2021-11-10 04:37:37 +0530

    Signup implimented

--- routes/signup.js
+++ routes/signup.js
@@ -9,17 +9,22 @@ Router.post("/",(req,res)=> {
     let SQL = "INSERT INTO userLogin ( `email`, `password`, `user_name`) VALUES (?, ?, ?);"
     mysqlConnection.query(`select * from userLogin where email="${req.body.email}";`, (err,rows,fields)=> {
         if(!err) {
-            if(rows.length > 0)
-            mysqlConnection.query(SQL,[req.body.email,req.body.password,req.body.userName], (err,rows,fields)=> {
-                if(!err){
-                    res.send({code: 200})
-                    console.log("updated successfully")
-                }
-                else {
-                    res.send({message:JSON.stringify(err)})
-                    console.log(err)
-                }
-            })
+            console.log(rows)
+            if(rows.length === 0){
+                mysqlConnection.query(SQL,[req.body.email,req.body.password,req.body.userName], (err,rows,fields)=> {
+                    if(!err){
+                        res.send({
+                            code: 200,
+                            successfull: true
+                        })
+                        console.log("updated successfully")
+                    }
+                    else {
+                        res.send({message:JSON.stringify(err)})
+                        console.log(err)
+                    }
+                })
+            }
             else {
                 res.send({
                     code: 400,
